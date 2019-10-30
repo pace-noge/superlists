a6dd4bb2c75b32138d657058d66f215df392f90f
nasa <nasa.freaks@gmail.com>
2019-10-30 16:55:59 +0700

    new url + view for adding to  existing lists. FT passes :)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -119,15 +119,15 @@ class NewVisitorTest(LiveServerTestCase):
 
 
         # Francis get his own unique URL
-        francis_list_url = self.browser_current_url
+        francis_list_url = self.browser.current_url
         self.assertRegex(francis_list_url, "/lists/.+")
         self.assertNotEqual(francis_list_url, edith_list_url)
 
         # Again there is no trace of Edith's list
-        page_text = self.browser.find_element_by_tag_name("body").text()
+        page_text = self.browser.find_element_by_tag_name("body").text
         self.assertNotIn("Buy peacock feathers", page_text)
         self.assertIn("Buy milk", page_text)
 
         # satisfied, they both go back to sleep
 
-        self.fail('Finish the test.')
+        # self.fail('Finish the test.')
