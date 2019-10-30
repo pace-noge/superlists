fb89a96a244920a31b5f5d8dfe4e620eb31e441a
nasa <nasa.freaks@gmail.com>
2019-10-30 10:36:19 +0700

    Make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -21,7 +21,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has jeard about a cool new online to-do app. She goes
         # to check it out its homepage
-        self.browser.get("http://localhost:8000")
+        self.browser.get(self.live_server_url)
 
         # She notice the page title and header mention to-do lists
         self.assertIn("To-Do", self.browser.title)
@@ -52,7 +52,7 @@ class NewVisitorTest(unittest.TestCase):
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make fly" (Edith is very methodical)
         input_box = self.browser.find_element_by_id('id_new_item')
-        input_box.send_keys("Use peacock feathers to make fly")
+        input_box.send_keys("Use peacock feathers to make a fly")
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
 
@@ -70,8 +70,3 @@ class NewVisitorTest(unittest.TestCase):
         # Satisfied, she goes back to sleep
 
         self.fail('Finish the test.')
-
-
-if __name__ == "__main__":
-    unittest.main(warnings='ignore')
-
